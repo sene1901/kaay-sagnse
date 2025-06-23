9997bda4dc9506396d38aa50810f1e0df1b36de3
sene32304@gmail.com <sene32304@gmail.com>
2025-06-23 15:20:23 +0000

    premier commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Page from './composant/Page'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ab ea dolor ipsam suscipit aut consequatur dolore est fugiat quos. 
+      Deleniti itaque facere iste numquam qui saepe, deserunt quas quam.</p>
+    
+    </div> 
+
+    <Page />
+    </>
+  )
+}
+
+export default App
---
+++ src/composant/Page.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function Page() {
+
+  return (
+    <>
+     <div className='nom'>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ab ea dolor ipsam suscipit aut consequatur dolore est fugiat quos. </p>
+    </div> 
+    </>
+  )
+}
+
+export default Page
