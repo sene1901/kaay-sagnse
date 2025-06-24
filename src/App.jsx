d5d657cdc8eb94008b1943e630f7d47ba8df8cd7
sene32304@gmail.com <sene32304@gmail.com>
2025-06-24 13:47:09 +0000

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+// import viteLogo from '/vite.svg
+import Navbar from './composant/Navbar'
+import Connexion from './composant/Connexion'
+import Inscription from './composant/Inscription'
 import Page from './composant/Page'
+import { Route,Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -9,12 +13,20 @@ function App() {
   return (
     <>
      <div>
+    <Navbar></Navbar>
+    <Routes>
+      <Route path='/' element={  <Page />} />
+      <Route path='/connexion' element={<Connexion></Connexion>} />
+      <Route path='/inscription' element={<Inscription></Inscription>} />
+    </ Routes>
+    <footer></footer>
+    
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ab ea dolor ipsam suscipit aut consequatur dolore est fugiat quos. 
       Deleniti itaque facere iste numquam qui saepe, deserunt quas quam.</p>
     
     </div> 
 
-    <Page />
+  
     </>
   )
 }
