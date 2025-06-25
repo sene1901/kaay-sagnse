639d6a132fb99bc44136c12c79e3e897c43dfbdd
sene32304@gmail.com <sene32304@gmail.com>
2025-06-25 13:46:39 +0000

    Accueil

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import Page from './composant/Page'
+// import Page from './composant/Page'
+// import './App.css'
+
+// function App() {
+
+//   return (
+//     <>
+//      <div>
+//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ab ea dolor ipsam suscipit aut consequatur dolore est fugiat quos. 
+//       Deleniti itaque facere iste numquam qui saepe, deserunt quas quam.</p>
+    
+//     </div> 
+
+//     <Page />
+//     </>
+//   )
+// }
+
+// export default App
+// import Navbar from './components/Navbar'
+// import Connexion from './components/Connexion'
+import Inscription from './components/Inscription'
+import Accueil from './components/Accueil'
+import { Route,Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -9,12 +32,14 @@ function App() {
   return (
     <>
      <div>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ab ea dolor ipsam suscipit aut consequatur dolore est fugiat quos. 
-      Deleniti itaque facere iste numquam qui saepe, deserunt quas quam.</p>
-    
+    {/* <Navbar></Navbar> */}
+    <Routes>
+      <Route path='/' element={  <Accueil />} />
+      {/* <Route path='/connexion' element={<Connexion></Connexion>} /> */}
+      <Route path='/inscription' element={<Inscription></Inscription>} />
+    </ Routes>
+    <footer></footer>
     </div> 
-
-    <Page />
     </>
   )
 }
